c514c7bb8fe6b62e91a88400dc748d957e0eb9c9
Christopher Fujino <christopherfujino@gmail.com>
2025-12-01 18:45:49 -0800

    bootstrap a JUCEApplication

--- cliSineWave/Source/Main.cpp
+++ cliSineWave/Source/Main.cpp
@@ -1,6 +1,12 @@
 #include <JuceHeader.h>
 
-int main(int argc, char *argv[]) {
-  // ..your code goes here!
-  return 0;
-}
+class App : public juce::JUCEApplication {
+public:
+  virtual const juce::String getApplicationName() { return "cli-xsynth"; }
+  virtual const juce::String getApplicationVersion() { return "0.1"; }
+
+  virtual void initialise(const juce::String &commandLineParameters) {}
+  virtual void shutdown() {}
+};
+
+START_JUCE_APPLICATION(App)
