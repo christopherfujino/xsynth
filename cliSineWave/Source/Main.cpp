1f0a4552f17b6ecaff70fb015cfbd6dd796a3956
Christopher Fujino <christopherfujino@gmail.com>
2025-12-01 19:21:53 -0800

    fix memory leaks

--- cliSineWave/Source/Main.cpp
+++ cliSineWave/Source/Main.cpp
@@ -2,10 +2,17 @@
 
 using namespace juce;
 
-AudioDeviceManager devmgr;
-AudioFormatManager fmgr;
+// TODO figure out assertions in
+// JUCE Assertion failure in juce_MidiDeviceListConnectionBroadcaster.cpp:48
+// JUCE Assertion failure in juce_AsyncUpdater.cpp:78
+// JUCE Assertion failure in juce_MidiDeviceListConnectionBroadcaster.cpp:54
+//
+// To do with JUCE_ASSERT_MESSAGE_THREAD
 
 int main(void) {
+  AudioDeviceManager devmgr;
+  AudioFormatManager fmgr;
+
   fmgr.registerBasicFormats();
   ScopedPointer<ToneGeneratorAudioSource> source =
       new ToneGeneratorAudioSource();
@@ -13,15 +20,18 @@ int main(void) {
   // 0 in, 2 out
   devmgr.initialiseWithDefaultDevices(0, 2);
   AudioIODevice *device = devmgr.getCurrentAudioDevice();
-  if (device && source) {
-    source->prepareToPlay(device->getDefaultBufferSize(),
-                          device->getCurrentSampleRate());
-    ScopedPointer<AudioSourcePlayer> player = new AudioSourcePlayer();
-    player->setSource(source);
-    devmgr.addAudioCallback(player);
-    Thread::sleep(1000);
-    source->releaseResources();
-    return 0;
+  if (!device || !source) {
+    return -1;
   }
-  return -1;
+
+  source->prepareToPlay(device->getDefaultBufferSize(),
+                        device->getCurrentSampleRate());
+  ScopedPointer<AudioSourcePlayer> player = new AudioSourcePlayer();
+  player->setSource(source);
+  devmgr.addAudioCallback(player);
+  Thread::sleep(1000);
+
+  source->releaseResources();
+  devmgr.closeAudioDevice();
+  fmgr.end();
 }
