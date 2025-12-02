e57764bbc4d67c1fb9d8c5ff79e4f180e7423de4
Christopher Fujino <christopherfujino@gmail.com>
2025-12-01 18:39:14 -0800

    clean

--- cliSineWave/Source/Main.cpp
+++ cliSineWave/Source/Main.cpp
@@ -1,19 +1,6 @@
-/*
-  ==============================================================================
-
-    This file contains the basic startup code for a JUCE application.
-
-  ==============================================================================
-*/
-
 #include <JuceHeader.h>
 
-//==============================================================================
-int main (int argc, char* argv[])
-{
-
-    // ..your code goes here!
-
-
-    return 0;
+int main(int argc, char *argv[]) {
+  // ..your code goes here!
+  return 0;
 }
