fc8179343ccb19f70f6e943a0fb32b3875db49d7
Christopher Fujino <christopherfujino@gmail.com>
2025-12-03 14:56:51 -0800

    fix all asserts

--- cliSineWave/main.cpp
+++ cliSineWave/main.cpp
@@ -1,32 +1,43 @@
-#include <JuceHeader.h>
 #include "pitches.h"
+#include <JuceHeader.h>
 
 using namespace juce;
 
 int main() {
-  AudioDeviceManager devmgr;
-  AudioFormatManager fmgr;
+  // This must be called manually if you are not using the
+  // START_JUCE_APPLICATION macro, even if you are not using a GUI.
+  initialiseJuce_GUI();
 
-  fmgr.registerBasicFormats();
-  auto source = std::make_unique<ToneGeneratorAudioSource>();
+  // Create an arbitrary block so that our AudioDeviceManager is deconstructed
+  // before we shutdownJuce_GUI()
+  {
+    AudioDeviceManager devmgr;
+    AudioFormatManager fmgr;
 
-  // 0 in, 2 out
-  devmgr.initialiseWithDefaultDevices(0, 2);
-  AudioIODevice *device = devmgr.getCurrentAudioDevice();
-  if (!device || !source) {
-    return -1;
+    fmgr.registerBasicFormats();
+    auto source = std::make_unique<ToneGeneratorAudioSource>();
+
+    // 0 in, 2 out
+    devmgr.initialiseWithDefaultDevices(0, 2);
+    AudioIODevice *device = devmgr.getCurrentAudioDevice();
+    if (!device || !source) {
+      return -1;
+    }
+
+    source->prepareToPlay(device->getDefaultBufferSize(),
+                          device->getCurrentSampleRate());
+    auto player = std::make_unique<AudioSourcePlayer>();
+    player->setSource(source.get());
+    source->setAmplitude(0.5);
+    source->setFrequency(Pitches::nameA4);
+    const auto sleep = 200;
+
+    devmgr.addAudioCallback(player.get());
+    Thread::sleep(sleep);
+    source->setFrequency(Pitches::nameC5);
+    Thread::sleep(sleep);
+
+    devmgr.closeAudioDevice();
   }
-
-  source->prepareToPlay(device->getDefaultBufferSize(),
-                        device->getCurrentSampleRate());
-  auto player = std::make_unique<AudioSourcePlayer>();
-  player->setSource(source.get());
-  source->setAmplitude(0.5);
-  source->setFrequency(Pitches::nameA4);
-  const auto sleep = 200;
-
-  devmgr.addAudioCallback(player.get());
-  Thread::sleep(sleep);
-  source->setFrequency(Pitches::nameC5);
-  Thread::sleep(sleep);
+  shutdownJuce_GUI();
 }
